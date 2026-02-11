899a949a15b63890dd2895d0ed83da4892fd9309
KwonJuHwan <118177454+KwonJuHwan@users.noreply.github.com>
2026-02-12 00:13:14 +0900

    feat: sumAmountByMemberIdAndDateBetween 조건중 날짜를 하루단위로 조회하게끔 default method 등록

--- src/main/java/com/saveme/ledger/repository/ExpenseRepository.java
+++ src/main/java/com/saveme/ledger/repository/ExpenseRepository.java
@@ -2,7 +2,17 @@ package com.saveme.ledger.repository;
 
 import com.saveme.ledger.domain.Expense;
 import com.saveme.ledger.repository.custom.ExpenseRepositoryCustom;
+import java.time.LocalDate;
+import java.time.LocalTime;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ExpenseRepository extends JpaRepository<Expense, Long>, ExpenseRepositoryCustom {
+
+    default Long sumAmountByMemberIdAndDate(Long memberId, LocalDate date) {
+        return sumAmountByMemberIdAndDateBetween(
+            memberId,
+            date.atStartOfDay(),
+            date.atTime(LocalTime.MAX)
+        );
+    }
 }
