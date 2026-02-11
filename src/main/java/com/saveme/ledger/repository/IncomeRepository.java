0e4b0c64b5897350e643d67a4281223e23c3734a
KwonJuHwan <118177454+KwonJuHwan@users.noreply.github.com>
2026-02-11 18:07:37 +0900

    feat: IncomeRepository 구현 및 member를 통한 수입 출력 메소드 구현

---
+++ src/main/java/com/saveme/ledger/repository/IncomeRepository.java
@@ -0,0 +1,13 @@
+package com.saveme.ledger.repository;
+
+import com.saveme.ledger.domain.Income;
+import com.saveme.ledger.repository.custom.IncomeRepositoryCustom;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface IncomeRepository extends JpaRepository<Income, Long>, IncomeRepositoryCustom {
+
+    List<Income> findAllByMemberIdOrderByDateDesc(Long memberId);
+
+}
