3fa3205296dfdcbd3c343afb1afd4b3090e72f26
KwonJuHwan <118177454+KwonJuHwan@users.noreply.github.com>
2026-02-11 18:22:19 +0900

    feat: CategoryRepository 구현 및 3가지 default 메소드 구현

---
+++ src/main/java/com/saveme/ledger/repository/CategoryRepository.java
@@ -0,0 +1,13 @@
+package com.saveme.ledger.repository;
+
+import com.saveme.ledger.domain.Category;
+import java.util.List;
+import java.util.Optional;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CategoryRepository extends JpaRepository<Category, Long> {
+    Optional<Category> findByNameAndParentIsNull(String name);
+
+    List<Category> findAllByParentIsNull();
+    List<Category> findAllByParentId(Long parentId);
+}
